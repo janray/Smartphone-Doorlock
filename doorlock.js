f1e4344ed49f870d1f27f8a525dee52c934ad653
Jan Ray C Rulida <jrcrulida@gmail.com>
2020-02-15 19:51:29 +0800

    Added some blocking delays for activating the servos and linear actuators.

--- doorlock.js
+++ doorlock.js
@@ -48,6 +48,7 @@ var buttonPin = 4;
 var ledPin = 17;
 var extendLinearActuatorPin = 18;
 var retractLinearActuatorPin = 27;
+var sleep = require('sleep');
 
 var blynk_Token = require('./token.js');
 var blynkToken = blynk_Token.blynk_token;
@@ -68,13 +69,12 @@ var Gpio = require('pigpio').Gpio,
   }),
   led = new Gpio(ledPin, {mode: Gpio.OUTPUT}),
   extendActuator = new Gpio(extendLinearActuatorPin, {mode: Gpio.OUTPUT}),
-  retractActuator = new Gpio(retractLinearActuatorPin, {mode: Gpio.OUTPUT});
+  retractActuator = new Gpio(retractLinearActuatorPin, {mode: Gpio.OUTPUT})
 
 //Setup blynk
 var Blynk = require('blynk-library');
 //var blynk = new Blynk.Blynk(blynkToken);
 var AUTH = blynkToken;
-//var blynk = new Blynk.Blynk(AUTH, options= {addr:"192.168.254.135", port:9443});
 
 /* The next line enables this script to connect to the local blynk server on the RaspberryPi */
 var blynk = new Blynk.Blynk(AUTH, options = { connector : new Blynk.TcpClient( options = { addr:"127.0.0.1", port:8080 } ) });
@@ -82,26 +82,74 @@ var blynk = new Blynk.Blynk(AUTH, options = { connector : new Blynk.TcpClient( o
 
 var v0 = new blynk.VirtualPin(0);
 
-console.log("locking door")
-lockDoor()
+console.log("locking door");
 
-button.on('interrupt', function (level) {
+(async () => {
+	lockDoor();
+	await delay(19000);
+	motor.servoWrite(lockedState);
+	blynk.notify("Door has been locked!");
+	//After 1.5 seconds, the door lock servo turns off to avoid stall current
+	setTimeout(function(){motor.servoWrite(0)}, 1500);
+})();
+
+
+button.on('interrupt', (level) => {
 	console.log("level: " + level + " locked: " + locked)
 	if (level == 0) {
 		if (locked) {
-			unlockDoor()
+			//unlockDoor()
+			motor.servoWrite(unlockedState);
+			//After 1.5 seconds, the door lock servo turns off to avoid stall current
+			setTimeout(function(){motor.servoWrite(0)}, 1500);
+			(async () => {
+				
+				await delay(900);
+				unlockDoor();
+			})();
 		} else {
-			lockDoor()
+			//lockDoor()
+			 if (locked === true) {
+				motor.servoWrite(0);
+			 }else { 
+				 	lockDoor();
+					(async () => {
+							await delay(19000);
+							motor.servoWrite(lockedState);
+							//After 1.5 seconds, the door lock servo turns off to avoid stall current
+							setTimeout(function(){motor.servoWrite(0)}, 1500);
+							blynk.notify("Door has been locked!");
+					})();
+				}
+			
 		}
 	}
 });
 
+
 v0.on('write', function(param) {
 	console.log('V0:', param);
-  	if (param[0] === '0') { //unlocked
-  		unlockDoor()
+	  if (param[0] === '0') { //unlocked
+		(async () => {
+			motor.servoWrite(unlockedState);
+			//After 1.5 seconds, the door lock servo turns off to avoid stall current
+			setTimeout(function(){motor.servoWrite(0)}, 1500);
+			await delay(900);
+			unlockDoor();
+		})();
   	} else if (param[0] === '1') { //locked
-  		lockDoor()
+		  if (locked === true) {
+			  motor.servoWrite(0);
+		  } else {
+				(async () => {
+						lockDoor();
+						await delay(19000);
+						motor.servoWrite(lockedState);
+						//After 1.5 seconds, the door lock servo turns off to avoid stall current
+						setTimeout(function(){motor.servoWrite(0)}, 1500);
+						blynk.notify("Door has been locked!");
+				})();
+		  }
   	} else {
   		blynk.notify("Door lock button was pressed with unknown parameter");
   	}
@@ -110,36 +158,29 @@ v0.on('write', function(param) {
 blynk.on('connect', function() { console.log("Blynk ready."); });
 blynk.on('disconnect', function() { console.log("DISCONNECT"); });
 
+// CLOSE
 function lockDoor() {
-	motor.servoWrite(lockedState);
+	
 	led.digitalWrite(1);
 	locked = true;
 
-	//notify
-  	blynk.notify("Door has been locked!");
-  	
-  	//After 1.5 seconds, the door lock servo turns off to avoid stall current
-	setTimeout(function(){motor.servoWrite(0)}, 1500);
-	(async () => {
-		await delay(2000);
-		retractActuator.digitalWrite(0);
-		extendActuator.digitalWrite(1);
-	})();
+	extendActuator.digitalWrite(1);
+	retractActuator.digitalWrite(0);
+	
 }
 
+// OPEN
 function unlockDoor() {
-	motor.servoWrite(unlockedState);
+
 	led.digitalWrite(0);
 	locked = false;
+
+	extendActuator.digitalWrite(0);
+	retractActuator.digitalWrite(1);
 
 	//notify
   	blynk.notify("Door has been unlocked!"); 
 
-  	//After 1.5 seconds, the door lock servo turns off to avoid stall current
-	setTimeout(function(){motor.servoWrite(0)}, 1500);
-	(async () => {
-		await delay(2000);
-		extendActuator.digitalWrite(0);
-		retractActuator.digitalWrite(1);
-	})();
+  	
+
 }
